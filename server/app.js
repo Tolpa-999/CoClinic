b980df1ba4a1475a01cec9e69a93086e5d49f18c
Tolpa-999 <mahmudtolpa666@gmail.com>
2025-05-05 15:00:44 +0300

    remove multer-storage-cloudinary~

--- server/app.js
+++ server/app.js
@@ -67,15 +67,15 @@ app.use(cookieParser())
 
 // cludinary instead of local files
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 
 
 
-const upload = multer({ storage });
+// const upload = multer({ storage });
 
 
 app.use("/api/auth/users", authRoutes);
